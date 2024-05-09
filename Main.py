45970cc34ee742102e6a98439ca0fc029ff28e28
bhiago19 <test@example.com>
2024-05-09 14:39:48 -0300

    Initial commit

---
+++ Main.py
@@ -0,0 +1,15 @@
+class Main:
+    pass
+
+print("Testando Projeto")
+
+from Cliente import Cliente
+from Conta import Conta
+
+c1 = Cliente("Jão", "8888-8888")
+conta=Conta(c1.nome,4545,0)
+
+print(c1)
+print(c1.nome, " e ", c1.telefone)
+print(conta.titular," Numero: ", conta.numero, "Seu saldo: ", conta.saldo)
+
