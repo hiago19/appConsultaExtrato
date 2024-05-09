e95529d6c4fbb53f8935f3eac3412c67c3d9f96d
bhiago19 <test@example.com>
2024-05-09 16:45:36 -0300

    second commit

--- Conta.py
+++ Conta.py
@@ -1,16 +1,29 @@
 class Conta:
     def __init__(self, titular, numero, saldo):
-        self.saldo=0
-        self.numero=numero
-        self.titular=titular
+        self._saldo=0.0
+        self._numero=numero
+        self._titular=titular
 
         @property
-        def get_saldo(self):
+        def saldo(self):
             return self._saldo
 
         @saldo.setter
-        def set_saldo(self, saldo):
+        def saldo(self, saldo):
             if(saldo < 0):
                 print("O saldo não pode ser negativo!")
             else:
                 self._saldo = saldo
+
+    def saque(self, valor):
+        if(self._saldo>=valor):
+            self._saldo-=valor
+            print("Saque realizado com sucesso!")
+        else:
+            print("Saldo insuficiente!")
+
+    def deposita(self, valor):
+        self._saldo+=valor
+
+    def extrato(self):
+        print("Cliente: ", self._titular, " Saldo Atual: ", self._saldo)
--- Main.py
+++ Main.py
@@ -7,9 +7,13 @@ from Cliente import Cliente
 from Conta import Conta
 
 c1 = Cliente("Jão", "8888-8888")
-conta=Conta(c1.,4545,0)
+conta=Conta(c1.get_nome(), 4545, 0)
 
+'''
 print(c1)
 print(c1.nome, " e ", c1.telefone)
 print(conta.titular," Numero: ", conta.numero, "Seu saldo: ", conta.saldo)
-
+'''
+conta.deposita(int(input("Insira o valor depositado: ")))
+conta.saque(int(input("Insira o valor do saque: ")))
+conta.extrato()
